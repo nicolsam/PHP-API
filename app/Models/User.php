c061ce54a9eb734db20f7de97a48ed17757f8cdd
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-25 22:18:30 -0300

    Nome da função de selecionar usuário melhorada para melhor interpretação

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,7 @@ use \PDO;
 class User {
     private static $table = 'users';
 
-    public static function get($id) {
-        // $obDatabase = new Database(self::$table);
-
-        // $obDatabase->select('WHERE id =');
-
+    public static function selectUser($id) {
         return (new Database(self::$table))->select($id)->fetchAll(PDO::FETCH_CLASS, self::class);
    }
 }
