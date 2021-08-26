b6a7a3a0a69899671662ac1fc8c77f24c7153d14
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-25 22:52:42 -0300

    Content-Type para application/json

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+header('Content-Type: application/json');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap/app.php';
 
