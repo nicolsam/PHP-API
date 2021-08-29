e92e47ec2c606bae1f8ceb77856230260e159896
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-29 19:26:35 -0300

    Index utilizando classe API

--- public/index.php
+++ public/index.php
@@ -4,56 +4,13 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap/app.php';
 
-use \App\Models\User;
-use \Exception;
+use App\Http\Api;
 
-if(!$_GET['url']) {
-    die('Variável url não existe');
-}
+$obApi = new Api($_GET['url']);
 
-$url = explode('/', $_GET['url']);
-
-if(!$url[0] === 'api') {
-    die('A URI da API não está sendo corretamente utilizada');
-}
-
-array_shift($url);
-$service =  'App\Services\\' . ucfirst($url[0]) . 'Service';
-
-array_shift($url);
-$method = strtolower($_SERVER['REQUEST_METHOD']);
-
-$data = [
-    'status' => 'failed',
-    'data' => null,
-];
-
-try {
-    
-    $response = call_user_func_array([new $service, $method], $url);
-
-    if(!$response) {
-        throw new Exception("This user doesn't exist!");
-        exit;
-    }
-
-    http_response_code(200);
-
-    $data['status'] = 'success';
-    $data['data'] = $response;
-
-    echo json_encode($data);
-    exit;
-
-} catch(Exception $error) {
-    http_response_code(404);
-
-    $data['data'] = $error->getMessage();
-    echo json_encode($data, JSON_UNESCAPED_UNICODE);
-    exit;
-}
+echo $obApi->run();
