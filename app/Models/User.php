97a2ff8371ce58ca541b47e4784375b66cd5f8f8
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-29 00:37:59 -0300

    Adicionada função de adicionar dados no BD pelo método POST

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,41 @@ use \App\Db\Database;
 use \PDO;
 
 class User {
+    private static $id;
+
     private static $table = 'users';
 
+    /**
+     * Seleciona um usuário especifico do Banco de dados
+     *
+     * @param integer $id
+     *
+     * @return Database
+     */
     public static function selectUser($id) {
         return (new Database(self::$table))->select($id)->fetchAll(PDO::FETCH_CLASS, self::class);
     }
 
+    /**
+     * Seleciona todos os usuários que estão presentes no Banco de dados
+     *
+     * @return Database
+     */
     public static function selectAllUsers() {
         return (new Database(self::$table))->selectAll()->fetchAll(PDO::FETCH_CLASS, self::class);
+    }
+    
+    /**
+     * Insere no Banco de dados o usuário requisitado (email/password/name)
+     *
+     * @param POST $data
+     * @return Database
+     */
+    public static function insert($data) {
+        $obDatabase = new Database(self::$table);
+
+        self::$id = $obDatabase->insert($data);
+
+        return true;
     }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -15,7 +15,9 @@ class UserService {
     }
 
     public function post() {
-        
+        $data = $_POST;
+        print_r($data);
+        return User::insert($data);
     }
 
     public function update() {
